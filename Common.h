81a65cea38617bbc1527c78b825d1fc6e8ef1fc5
Pass1onfruit <39490965+Pass1onfruit@users.noreply.github.com>
2020-07-19 18:05:29 +0800

    Update Common.h

--- Common.h
+++ Common.h
@@ -2,44 +2,66 @@
 #pragma once  //保证每个头文件只被包含一次//
 #include <iostream>
 #include <assert.h>
+#include <map>
+#include <Windows.h>
+#include <mutex>
 
-using namespace std;
+//using namespace std;
+using std::cout;
+using std::endl;
 
 //个数都有num，字节数都用size
 const size_t MAX_SIZE = 64 * 1024;//64k //用const不用宏，调试时可以看见值
 const size_t NFREE_LIST = MAX_SIZE / 8;//free_list的大小
+const size_t PAGE_SHIFT = 12;//4k为页移位
 const size_t MAX_PAGES = 129;//0~16找central cache调，没有锁没有竞争并发性能高，17~128页找page cache，超过128就直接找系统
-const size_t PAGE_SHIFT = 12; //4k为页移位
+//超过128页就没有这个内存池的概念了
+const size_t PAGE_SHIFT = 12; // 4k为页移位
+
+inline static void* system_alloc(size_t numpage)
+{
+#ifdef _WIN32
+	void* ptr = VirtualAlloc(0, numpage * (1 << PAGE_SHIFT),
+		MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+
+#endif //  _WIN32
+	if(ptr == nullptr)
+		throw std::bad_alloc();//申请失败了抛异常，一般不会
+	return ptr;
+}
 
 inline void*& next_obj(void* obj)//取下一个对象//内联
 {
-	return* ((void**)obj);
+	return *((void**)obj);
 }
 
 class free_list//自由链表储存申请了的内存//头删最好O(1)
 {
 public:
-	void _push(void* obj)//还空间时//头插自由链表
+	void _push(void* obj)//还空间时
 	{
+		//头插自由链表
 		next_obj(obj) = _freelist;
 		_freelist = obj;
+		++_num;
+	}
+	void* _pop()//拿一个对象出来//取走对象时需要
+	{
+		//头删
+		void* obj = _freelist;//_freelist指向的第一个
+		_freelist = next_obj(obj);//_freelist指向下一个
+		--_num;
 	}
 
-	void _push_range(void* head, void* tail)//范围push
+	void _push_range(void* head, void* tail, size_t num)//范围push
 	{
 		next_obj(tail) = _freelist;
 		_freelist = head;
+		_num += num;
 	}
-
-	void* _pop()//拿一个对象出来//取走对象时需要// 头删
+	size_t _pop_range(void*& start, void*& end, size_t num)
 	{
-		void* obj = _freelist;//_freelist指向的第一个
-		_freelist = next_obj(obj);
-	}
-
-	size_t _pop_range(void* start, void* end, size_t num)
-	{
-		size_t actual_num;
+		size_t actual_num = 0;
 		void* prev = nullptr;//prev是cur的前一位
 		void* cur = _freelist;
 		for (;(actual_num < num) && (cur != nullptr); ++actual_num)//走到最后cur指向空了prev指向尾
@@ -50,29 +72,29 @@ public:
 		start = _freelist;
 		end = prev;
 		_freelist = cur;
-		
-		return actual_num;
+		_num -= actual_num; 
+		return actual_num;//实际获取到的数量
 	}
 
-	bool _empty()
+	size_t num()
+	{
+		return _num;
+	}
+
+	void _clear()//清理内存块
+	{
+		_freelist = nullptr;
+		_num = 0;
+	}
+
+	bool _empty()//判空
 	{
 		return _freelist == nullptr;
 	}
 
 private:
 	void* _freelist    = nullptr;//默认初始化为空
-};
-
-class size_class
-{
-public:
-	static size_t list_index(size_t size)//index内存在自由链表中的位置
-	{
-		if (size % 8 == 0)//8的整数倍
-			return size / 8 - 1;
-		else
-			return size / 8;
-	}
+	size_t _num = 0;
 };
 
 //central cache
@@ -82,7 +104,7 @@ typedef unsigned int PAGE_ID;
 #else
 typedef unsigned long long PAGE_ID;//long long // 2^64
 #endif
-/////
+
 #ifdef _X86
 typedef unsigned int PAGE_ID;
 #else
@@ -91,16 +113,17 @@ typedef unsigned long long PAGE_ID;//long long // 2^64
 
 struct span
 {
-	PAGE_ID _pageid;//页号 //不能直接用整形 64位下存不下
-	int _pagesize;//页数量
+	PAGE_ID _pageid = 0;//页号 //不能直接用整形 64位下存不下
+	PAGE_ID _pagesize = 0;//页数量
+
 	free_list _freelist;//对象自由链表
-	int _usecount;//内存块对象使用计数
-	// size_t objsize;//对象大小
-	span* _next;//双向链表 //span之间互相连接
-	span* _prev;//
+	int _usecount = 0;//内存块对象使用计数
+	size_t _obj_size = 0;//自由链表对象大小
+	span* _next = nullptr;//双向链表 //span之间互相连接
+	span* _prev = nullptr;//
 };
 
-class span_list
+class span_list //带头双向循环
 {
 public:
 	span_list()
@@ -109,7 +132,6 @@ public:
 		_head->_next = _head;
 		_head->_prev = _head;
 	}
-
 	span* begin()
 	{
 		return _head->_next;
@@ -142,7 +164,7 @@ public:
 		_insert(_head, newspan);
 	}
 
-	void pop_back()
+	void pop_back()//删尾
 	{
 		_erase(_head->_prev);
 	}
@@ -152,41 +174,57 @@ public:
 		_insert(_head->_next, newspan);
 	}
 
-	void pop_front()
+	void pop_front()//删头
 	{
 		_erase(_head->_next);
 	}
 
+	bool _empty()
+	{
+		return begin() == end();
+	}
+
+	void _lock()
+	{
+		_mtx.lock();
+	}
+
+	void _unlock()
+	{
+		_mtx.unlock();
+	}
+
 private:
 	span* _head;
+	std::mutex _mtx;
 };
 
 //控制大概12%的内存碎片
 class size_class
 {
 public:
-	// 控制在[1%，10%]左右的内碎片浪费
-	//8倍增长                    
-	// 在[1,128]范围时 以8byte对齐 freelist[0,16) //到16是开区间 //以8字节对齐有128/8=16个自由链表
-	// [129,1024] 16byte对齐 freelist[16,72)	  //以16字节对齐有(1024-128)/16 = 56 56+之前的16=72//减去128这个范围已经算过了// 15 / (128 + 16) = 10%最高浪费// 15 / 1024 = 1%最小浪费
-	// [1025,8*1024] 128byte对齐 freelist[72,128) //8*1024-1024=56 56+72=128// 127 / (1024 + 128) = 11%最高浪费
-	// [8*1024+1,64*1024] 1024byte对齐 freelist[128,184) //
-	static size_t _round_up(size_t size, int align)//alignment对其数
-	{
-		return (size + align - 1) & (~(align - 1));
-	}
-
 	// [9-16] + 7 = [16-23] -> 16 8 4 2 1
 	// [17-32] + 15 = [32,47] ->32 16 8 4 2 1
-	static inline size_t round_up(size_t size)
+	static size_t _round_up(size_t size, size_t align)//alignment 对齐数
 	{
-		//if (size % 8 != 0)//效率低
+		return (size + align - 1) & (~(align - 1));
+		//效率低
+		//if (size % 8 != 0)
 		//{
 		//	return (size / 8 + 1) * 8;
 		//}
 		//else
 		//	return size / 8;
-		//除法的效率是最低的，要转换成乘法，除的越小代价越大，乘法要转换成加法//加法最快
+		//除法的效率是最低的，除的越小代价越大，乘法是转换成加法//加法最快
+	}
+	// 控制在[1%，10%]左右的内碎片浪费
+	// 8倍增长
+	// 在[1,128]范围时 以8byte对齐 freelist[0,16) //到16是开区间 //以8字节对齐有128/8=16个自由链表
+	// [129,1024] 16byte对齐 freelist[16,72)	  //以16字节对齐有(1024-128)/16 = 56 56+之前的16=72//减去128这个范围已经算过了// 15 / (128 + 16) = 10%最高浪费// 15 / 1024 = 1%最小浪费
+	// [1025,8*1024] 128byte对齐 freelist[72,128) //8*1024-1024=56 56+72=128// 127 / (1024 + 128) = 11%最高浪费
+	// [8*1024+1,64*1024] 1024byte对齐 freelist[128,184) //
+	static inline size_t round_up(size_t size)//向上对齐
+	{
 		assert(size <= MAX_SIZE);
 
 		if (size <= 128)
@@ -210,12 +248,7 @@ public:
 	}
 
 	// [9-16] + 7 = [16-23]
-	static size_t _list_index(size_t size, int align_shift)
-	{
-		return ((size + (1 << align_shift) - 1) >> align_shift) - 1;
-	}
-
-	static size_t list_index(size_t size)
+	static size_t _list_index(size_t size, size_t align_shift)
 	{
 		//if (size % 8 == 0)
 		//{
@@ -225,7 +258,11 @@ public:
 		//{
 		//	  return size / 8;
 		//}
-
+		return ((size + (1 << align_shift) - 1) >> align_shift) - 1;
+	}
+	//index内存在自由链表中的位置
+	static size_t list_index(size_t size)//要算是属于第几个自由链表的桶
+	{
 		assert(size <= MAX_SIZE);
 
 		// 每个区间有多少个链
@@ -237,6 +274,7 @@ public:
 		else if (size <= 1024)
 		{
 			return _list_index(size - 128, 4) + group_array[0];
+			//128的范围已经算过了，所以减掉
 		}
 		else if (size <= 8192)
 		{
@@ -251,24 +289,26 @@ public:
 		return -1;
 	}
 
-	//[2,512]之间
-	static size_t num_move_size(size_t size)
+
+
+	static size_t num_move_size(size_t size)//size：申请的大小
 	{
-		if (size == 0)
+		//把范围限制到[2, 512]
+		if (size == 0)//防止有除0错误
 			return 0;
-		int num = (int)(MAX_SIZE / size);
+		//int num = static_cast<int>(MAX_SIZE / size);
+		int num = MAX_SIZE / size;
 		if (num < 2)
 			num = 2;
-		if (num > 512)
+		if (num > 512)//最少给4k，一个页
 			num = 512;
 		return num;
 	}
-
 	//计算一次向系统获取几个页
 	static size_t num_move_page(size_t size)
 	{
 		size_t num = num_move_size(size);
-		size_t npage = num * size;
+		size_t npage = num * size;//所需的总大小
 
 		npage >>= 12;//除以4k
 		if (npage == 0)//最少返回一页
